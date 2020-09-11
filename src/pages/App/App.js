40fc624e0db5156843fd731521f2d43c4d1952c9
Will Crawford <will@Wills-MacBook-Pro.local>
2020-09-11 11:25:10 -0400

    remove Ben's files

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -5,29 +5,6 @@ import WillsThings from '../WillsThings/WillsThings'
 
 class App extends Component {
   state = { 
-    bensThings: [
-      { 
-        name: 'banana',
-        image: 'https://images.unsplash.com/photo-1587132137056-bfbf0166836e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        attributes: ['potassium-rich', 'yellow when good', 'not a taco', 'ring ring ring ring ring ring ring']
-      },
-      {
-        name: 'turkey',
-        image: 'https://images.unsplash.com/photo-1560011961-4ab41261de01?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-        attributes: ['such gobbles', 'good when stuffed', 'not a taco', 'why gravy exists']
-      },
-      {
-        name: 'taco',
-        image: 'https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80',
-        attributes: ['yummy in my tummy', 'salsas may vary', 'definitely a taco', 'very edible']
-      },
-      {
-        name: 'linux',
-        image: 'https://i.imgur.com/3BmfSOA.png',
-        attributes: ["not a taco", "not Windows", "not macOS", "Penguins?"] 
-      },
-
-    ],
     willsThings: [
       {
         name: "waffle",
