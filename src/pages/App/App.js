269c17e0c89f256764353c647a2817af6404f52c
Will Crawford <will@Wills-MacBook-Pro.local>
2020-09-11 11:17:27 -0400

    add WillsThings, WillThing, remove some of Ben things

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
 import './App.css';
-import BensThings from '../BensThings/BensThings'
+import WillsThings from '../WillsThings/WillsThings'
 
 class App extends Component {
   state = { 
@@ -28,7 +28,7 @@ class App extends Component {
       },
 
     ],
-    davidsThings: [
+    willsThings: [
       {
         name: "waffle",
         image: "https://i.imgur.com/C3PmdhO.jpg",  
@@ -49,29 +49,7 @@ class App extends Component {
         image: "https://i.imgur.com/q9zgcaP.jpg",  
         attributes: ["pastry?", "food?", "edible?", "Horrible when toasted!"], 
       },
-    ],
-    shahzadsThings: [
-      {
-        name: "functional programming",
-        image: "https://imgs.xkcd.com/comics/functional.png",  
-        attributes: ["efficient", "reusability", "not a taco", "beautiful"],
-      },
-      {
-        name: "React hooks",
-        image: "https://images.unsplash.com/photo-1518644730709-0835105d9daa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        attributes: ["useEffect", "useState", "not a taco", "state's best friend"], 
-      },
-      {
-        name: "BluBlockers",
-        image: "https://susanmaryetta.files.wordpress.com/2011/06/blublocker-the-hangover.png?w=584",
-        attributes: ["very attractive", "useful probably!", "good for viewing tacos", "such hip", "worn by all the cool kids"],
-      },
-      { 
-        name: "Healthy food", 
-        image: "https://i.imgur.com/gRxOxsA.jpg", 
-        attributes: ["makes you live a long time", "consistent poops", "probably tacos sometimes!", "farm to market"], 
-      },
-    ] 
+    ]
   } 
   render() {
     return (
@@ -82,16 +60,16 @@ class App extends Component {
             <>
               {/* All the <a> tags should live here */}
               <h2>All-The-Things</h2>
-              <a href="/themanliestthings">Ben's Things</a> 
+              <a href="/willsthings">Will's Things</a> 
             </>
           }
         />
         {/* All the <Route> components should live here */}
         <Route 
-          exact path='/themanliestthings'
+          exact path='/willsthings'
           render={() => 
-            <BensThings 
-              bensThings={this.state.bensThings}
+            <WillsThings 
+              willsThings={this.state.willsThings}
             />
         }/>
       </>
---
+++ src/pages/WillsThings/WillsThings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WillsThings = (props) => {
+    return (
+        <>
+        <h1>Test</h1>
+        </>
+    )
+    
+}
+export default WillsThings
